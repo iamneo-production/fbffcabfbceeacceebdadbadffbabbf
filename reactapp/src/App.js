6c2ccb6c8b6400dc502b94555bb1371126f5fda3
Iamneo <techadmin@iamneo.ai>
2023-08-07 17:51:29 +0000

    Backup Commit

---
+++ reactapp/src/App.js
@@ -0,0 +1,13 @@
+import "./App.css";
+import Stopwatch from "./components/Stopwatch/stopwatch";
+//import Stop from './stop'
+
+function App() {
+  return (
+    <div className="app">
+      <Stopwatch />
+    </div>
+  );
+}
+
+export default App;
--- reactapp/src/components/Stopwatch/stopwatch.jsx
+++ reactapp/src/components/Stopwatch/stopwatch.jsx
@@ -1,14 +1,60 @@
-import React from 'react';
-import useTimer from '../../hook/useTimer';
-import { formatTime } from '../../utils';
+import React, { useState, useEffect } from 'react';
 
-const Stopwatch = () => {
-  const { timer, isActive, isPaused, handleStart, handlePause, handleResume, handleReset } = useTimer(0)
+import "./App.css"
+
+function Stopwatch() {
+  const [timer, setTimer] = useState(0)
+  const [isActive, setIsActive] = useState(false)
+  const [isPaused, setIsPaused] = useState(false)
+  const [disabled, setDisabled] = useState(true)
+
+  function handleStart() {
+    setIsActive(true);
+    setIsPaused(true);
+    setDisabled(false)
+  }
+
+  function handlePause() {
+    setIsActive(false);
+    setIsPaused(false)
+  }
+
+  function handleResume() {
+    setIsActive(true);
+    setIsPaused(true)
+  }
+
+  function handleReset() {
+    setTimer(0);
+    setIsActive(false);
+    setIsPaused(false)
+    setDisabled(true)
+  }
+
+  function formatTime(time) {
+    const hours = Math.floor(time / 3600).toString().padStart(2, '0');
+    const minutes = Math.floor((time % 3600) / 60).toString().padStart(2, '0');
+    const seconds = (time % 60).toString().padStart(2, '0');
+    return `${hours}:${minutes}:${seconds}`;
+  }
+
+  useEffect(() => {
+    let interval = null;
+    if (isActive) {
+      interval = setInterval(() => {
+        setTimer((time) => time + 1);
+      }, 1000);
+    } else {
+      clearInterval(interval);
+    }
+    return () => clearInterval(interval);
+  }, [isActive]);
 
   return (
     <div className="app">
-      <h3>React Stopwatch</h3>
       <div className='stopwatch-card'>
+        <div className="container">
+        <h1>React Stopwatch</h1>
         <p data-testid="time">{formatTime(timer)}</p> 
         <div className='buttons'>
           {
@@ -19,11 +65,12 @@ const Stopwatch = () => {
                   <button onClick={handleResume}>Resume</button>
               )
           }
-          <button onClick={handleReset} disabled={!isActive} data-testid="reset">Reset</button>
+          <button onClick={handleReset} disabled={disabled} data-testid="reset">Reset</button>
+        </div>
         </div>
       </div>
     </div>
   );
-};
+}
 
 export default Stopwatch;
